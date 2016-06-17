29caa3f8f227938d544a4efa176279eeb0159219
Toni Jiménez <t.jimenez@escolesnuria.cat>
2016-06-17 14:52:09 +0200

    Update define.inc.php

--- define.inc.php
+++ define.inc.php
@@ -5,23 +5,20 @@
 		define('DS',DIRECTORY_SEPARATOR);
 		define('APP',ROOT.DS.'app'.DS);
 		define('SYS',ROOT.DS.'sys'.DS);
-        define ('APP_W',dirname($_SERVER['SCRIPT_NAME']));
+        	define ('APP_W',dirname($_SERVER['SCRIPT_NAME']));
 		// array to search classes
 		 $classesDir = array (
-    		SYS,
-    		APP,
-    		APP.'controllers/',
-    		APP.'models/',
-    		APP.'views/'
+	    		SYS,
+	    		APP,
+	    		APP.'controllers/',
+	    		APP.'models/',
+	    		APP.'views/'
     		);
 
 		function __autoload($class_name) {
     		global $classesDir;
-   
     		foreach ($classesDir as $directory) {
-      
         		if (file_exists($directory . $class_name . '.php')) {
-
            			 require_once ($directory . $class_name . '.php');
             			return;
         		}
